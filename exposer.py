4a61cedc9af062e6d61f4a4f969b1128f8785c74
Rabiu Hadi Salisu <94430863+rabiuhadisalisu@users.noreply.github.com>
2024-07-25 10:15:25 -0700

    Create exposer.py

---
+++ exposer.py
@@ -0,0 +1,14 @@
+from http.server import SimpleHTTPRequestHandler, HTTPServer
+
+class CustomHandler(SimpleHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write(b"This Container Is Handling Palazzo Asset")
+
+if __name__ == "__main__":
+    server_address = ('', 8000)
+    httpd = HTTPServer(server_address, CustomHandler)
+    print("Serving on port 8000")
+    httpd.serve_forever()
